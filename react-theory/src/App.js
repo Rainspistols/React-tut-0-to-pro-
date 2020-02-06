278cf0209c28790782432f0c5a0edc047b927d0d
Rainspistols <ffly@mail.ru>
2020-02-06 15:44:26 +0100

    updated project according to the lesson plan, topic - Routing and components

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -1,155 +1,38 @@
-import React, { Component } from "react";
-import Radium from "radium";
-import Car from "./Car/Car";
-import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
-// import Counter from "./Counter/Counter";
-import { Rout, Route } from "react-router-dom";
-import AboutPage from "./components/AboutPage/AboutPage";
-
-export const ClickedContext = React.createContext(false);
+import React, {Component} from 'react'
+import './App.scss'
+import {Route} from 'react-router-dom'
+import About from './About/About'
+import Cars from './Cars/Cars'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      clicked: false,
-      cars: [
-        { name: "Ford", year: 2018 },
-        { name: "Audi", year: 2016 },
-        { name: "Mazda 3", year: 2010 }
-      ],
-      pageTitle: "React components",
-      showCars: true
-    };
-  }
-
-  onChangeName(name, index) {
-    const car = this.state.cars[index];
-    car.name = name;
-    const cars = [...this.state.cars];
-    cars[index] = car;
-    this.setState({
-      cars
-    });
-  }
-
-  deleteHandler(index) {
-    const cars = [...this.state.cars];
-    cars.splice(index, 1);
-
-    this.setState({ cars });
-  }
-
-  toggleCarsHandler = () => {
-    this.setState({
-      showCars: !this.state.showCars
-    });
-  };
-
-  componentDidMount() {
-    console.log("App componentDidMount");
-  }
-
   render() {
-    const divStyle = {
-      textAlign: "center"
-    };
-    let Style = {
-      nav: {
-        fontSize: "20px",
-        ul: {
-          "list-style": "none",
-          padding: "0",
-          display: "flex",
-          "justify-content": "center",
-          li: {
-            margin: "0 20px"
-          },
-          a: {
-            color: "black",
-            textDecoration: "none"
-          }
-        }
-      }
-    };
-
-    // const btnStyle = {
-    //   width: 200,
-    //   height: 50,
-    //   transition: "background-color 0.3s",
-    //   backgroundColor: "transparent",
-    //   marginTop: 20,
-    //   ":hover": {
-    //     backgroundColor: "yellow",
-    //     cursor: "pointer"
-    //   }
-    // };
-
-    let cars = null;
-    if (this.state.showCars) {
-      cars = this.state.cars.map((car, index) => {
-        return (
-          <ErrorBoundary key={car + index}>
-            <Car
-              name={car.name}
-              year={car.year}
-              index={index}
-              onDelete={this.deleteHandler.bind(this, index)}
-              onChangeName={e => {
-                this.onChangeName(e.target.value, index);
-              }}
-            />
-          </ErrorBoundary>
-        );
-      });
-    }
 
     return (
-      <div className="App" style={divStyle}>
-        <nav className="nav" style={Style.nav}>
-          <ul style={Style.nav.ul}>
-            <li style={Style.nav.ul.li}>
-              <a href="/" style={Style.nav.ul.a}>
-                Home
-              </a>
+      <div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <a href="/">Home</a>
             </li>
-            <li style={Style.nav.ul.li}>
-              <a href="/about" style={Style.nav.ul.a}>
-                About
-              </a>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/cars">Cars</a>
             </li>
           </ul>
         </nav>
 
-        <Route path={"/"} exact render={() => <h1>Home Page</h1>} />
+        <hr/>
 
-        <AboutPage title={"About Page"} />
+        {/*localhost:3000*/}
+        <Route path="/" exact render={() => <h1>Home Page</h1>} />
+        <Route path="/about" component={About} />
+        <Route path="/cars" component={Cars} />
 
-        {/* <ClickedContext.Provider value={this.state.clicked}>
-          <Counter />
-        </ClickedContext.Provider> */}
-        {/* <hr /> */}
-        {/* <button style={btnStyle} onClick={this.toggleCarsHandler}>
-          Toggle cars
-        </button> */}
-
-        {/* <button onClick={() => this.setState({ clicked: true })}>
-          Change clicked
-        </button> */}
-
-        <div
-          style={{
-            width: "400",
-            margin: "auto",
-            paddingTop: "20px"
-          }}
-        >
-          {cars}
-        </div>
       </div>
     );
   }
 }
 
-export default Radium(App);
+export default App
--- react-theory/src/index.js
+++ react-theory/src/index.js
@@ -1,16 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from 'react-router-dom'
+import registerServiceWorker from './registerServiceWorker';
+
 
 const application = (
   <BrowserRouter>
-    <App title={"I am from props"} />
+    <App />
   </BrowserRouter>
-);
+)
 
-ReactDOM.render(application, document.getElementById("root"));
 
-serviceWorker.register();
+ReactDOM.render(application, document.getElementById('root'));
+registerServiceWorker();
