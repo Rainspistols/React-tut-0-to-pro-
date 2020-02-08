4c5aba1275ac74520ca849f1d80c90ab142d111a
Rainspistols <ffly@mail.ru>
2020-02-08 11:08:57 +0100

    dynamic Routes

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import "./App.scss";
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 import About from "./About/About";
 import Cars from "./Cars/Cars";
+import CarDetail from "./CarDetail/CarDetail";
 
 class App extends Component {
   render() {
@@ -39,9 +40,12 @@ class App extends Component {
 
         <hr />
         {/*localhost:3000*/}
-        <Route path="/" exact render={() => <h1>Home Page</h1>} />
-        <Route path="/about" component={About} />
-        <Route path="/cars" component={Cars} />
+        <Switch>
+          <Route path="/" exact render={() => <h1>Home Page</h1>} />
+          <Route path="/about" component={About} />
+          <Route path="/cars/:name" component={CarDetail} />
+          <Route path="/cars" component={Cars} />
+        </Switch>
       </div>
     );
   }
