3c91ff5bd0cdced666251ff85e7c3c06e324a82a
Rainspistols <ffly@mail.ru>
2020-02-08 11:22:57 +0100

    Route guards

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -6,6 +6,10 @@ import Cars from "./Cars/Cars";
 import CarDetail from "./CarDetail/CarDetail";
 
 class App extends Component {
+  state = {
+    isLoggedIn: false
+  };
+
   render() {
     return (
       <div>
@@ -39,13 +43,23 @@ class App extends Component {
         </nav>
 
         <hr />
+        <div style={{ textAlign: "center" }}>
+          <h3>is logged in {this.state.isLoggedIn ? "TRUE" : "FALSE"}</h3>
+          <button onClick={() => this.setState({ isLoggedIn: true })}>
+            Login
+          </button>
+        </div>
+
+        <hr />
         {/*localhost:3000*/}
         <Switch>
           <Route path="/" exact render={() => <h1>Home Page</h1>} />
-          <Route path="/about" component={About} />
+          {this.state.isLoggedIn ? (
+            <Route path="/about" component={About} />
+          ) : null}
           <Route path="/cars/:name" component={CarDetail} />
           <Route path="/cars" component={Cars} />
-          <Redirect to={'/'}/>
+          <Redirect to={"/"} />
           {/* <Route
             render={() => (
               <h1 style={{ color: "red", textAlign: "center" }}>
