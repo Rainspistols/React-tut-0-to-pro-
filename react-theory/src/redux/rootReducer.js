111e774e400f7da220a2feb9bf8f5362ec7a85a3
Rainspistols <ffly@mail.ru>
2020-02-11 22:09:12 +0100

    send params to actions(dispatch)

--- react-theory/src/redux/rootReducer.js
+++ react-theory/src/redux/rootReducer.js
@@ -10,6 +10,11 @@ export default function rootReducer(state = initialState, action) {
       return {
         counter: state.counter - 1
       };
+    case "ADD_NUMBER":
+      return {
+        counter: state.counter + action.payload
+      };
+    default:
+      return state;
   }
-  return state;
 }
