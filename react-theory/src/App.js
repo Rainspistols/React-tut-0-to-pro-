2bd9b77170a5bf2a0db10446038005379486146c
Rainspistols <ffly@mail.ru>
2020-02-11 21:02:22 +0100

    change state to redux state

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -3,26 +3,19 @@ import { connect } from "react-redux";
 import "./App.scss";
 
 class App extends Component {
-  
-  updateCounter(value) {
-    // this.setState({
-    //   counter: this.state.counter + value
-    // });
-  }
-
   render() {
     console.log("APP", this.props);
     return (
       <div className={"App"}>
-        <h1> 
+        <h1>
           Счетчик <strong>{this.props.counter}</strong>
         </h1>
 
         <hr />
 
         <div className="Actions">
-          <button onClick={() => this.updateCounter(1)}>Добавить 1</button>
-          <button onClick={() => this.updateCounter(-1)}>Вычесть 1</button>
+          <button onClick={this.props.onAdd}>Добавить 1</button>
+          <button onClick={this.props.onSub}>Вычесть 1</button>
         </div>
       </div>
     );
@@ -34,5 +27,11 @@ function mapStateToProps(state) {
     counter: state.counter
   };
 }
+function mapDispatchToProps(dispatch) {
+  return {
+    onAdd: () => dispatch({ type: "ADD" }),
+    onSub: () => dispatch({ type: "SUB" })
+  };
+}
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
