1b2b6387f6d2c149d3ed34e0042ea32d60266742
Rainspistols <ffly@mail.ru>
2020-02-11 19:19:50 +0100

    basics how Redux works

--- react-theory/redux.js
+++ react-theory/redux.js
@@ -1,48 +1,40 @@
-const redux = require('redux')
+const redux = require("redux");
 
 const initialState = {
   counter: 0
-}
+};
 
-// Reducer
+//Reducer
 const reducer = (state = initialState, action) => {
-
-  if (action.type === 'ADD') {
+  if (action.type === "ADD") {
     return {
       counter: state.counter + 1
-    }
-  }
-
-  if (action.type === 'SUB') {
+    };
+  } else if (action.type === "SUB") {
     return {
       counter: state.counter - 1
-    }
-  }
-
-  if (action.type === 'ADD_NUMBER') {
+    };
+  } else if (action.type === "ADD_NUMBER") {
     return {
-      counter: state.counter + action.value
-    }
+      counter: state.counter - action.value
+    };
   }
 
-  return state
+  return state;
+};
 
-}
-
-// Store
-const store = redux.createStore(reducer)
-
+//Store
+const store = redux.createStore(reducer);
 store.subscribe(() => {
-  console.log('Subscribe', store.getState())
-})
-
-// Actions
+  console.log("Subscribe", store.getState());
+});
+//Actions
 const addCounter = {
-  type: 'ADD'
-}
+  type: "ADD"
+};
 
-store.dispatch(addCounter)
+store.dispatch(addCounter);
 
-store.dispatch({ type: 'SUB' })
+store.dispatch({ type: "SUB" });
 
-store.dispatch({ type: 'ADD_NUMBER', value: 10 })
+store.dispatch({ type: "ADD_NUMBER", value: 10 });
