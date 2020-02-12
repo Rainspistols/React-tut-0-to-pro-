a782ae5df5a52c316dc401c532abccfc7445be5b
Rainspistols <ffly@mail.ru>
2020-02-12 16:08:31 +0100

    redux and react dev tools

--- react-theory/src/index.js
+++ react-theory/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.module.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reduxThunk from 'redux-thunk';
 import rootReducer from './redux/rootReducer';
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
 
 const loggerMiddleWare = store => next => action => {
   const result = next(action);
@@ -16,7 +23,7 @@ const loggerMiddleWare = store => next => action => {
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(loggerMiddleWare, reduxThunk)
+  composeEnhancers(applyMiddleware(loggerMiddleWare, reduxThunk))
 );
 
 const app = (
